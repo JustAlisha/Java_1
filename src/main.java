659a89ff799e33075f5780904742f4bd94c960ac
Test <bloodreighn965@gmail.com>
2024-02-16 00:15:36 +0300

    ignore

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+        public static void main(String[] args) {
+
+            int price = 13_676;
+            int bonus = price / 20;
+            System.out.println("Бонус:" + bonus);
+
+
+        }
+}
