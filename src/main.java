048d1378991c159f6194e2dd2509254ca4bc12a2
Test <bloodreighn965@gmail.com>
2024-02-23 21:28:25 +0300

    update

--- src/main.java
+++ src/main.java
@@ -1,10 +1,10 @@
 public class main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            int price = 13_676;
-            int bonus = price / 20;
-            System.out.println("Бонус:" + bonus);
+        int price = 13_676;
+        int bonus = price / 20;
+        System.out.println("Бонус:" + bonus);
 
 
-        }
+    }
 }
